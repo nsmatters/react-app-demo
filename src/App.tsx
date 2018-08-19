e3d55ebacb4ceed8b3bce6429e6d62e9ff9fb7a8
fengfanhua <fengfanhua@meituan.com>
2018-08-19 17:06:36 +0800

    init react typescript bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+import { Navbar } from 'react-bootstrap';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">React-Bootstrap</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
