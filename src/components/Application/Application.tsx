5a5e5bc9805684383f544bfe277334e3e53ffad2
fengfanhua <fengfanhua@meituan.com>
2018-09-02 00:38:34 +0800

    update

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 
 import { AppItem } from '../AppItem/AppItem'
-import { Label, Table } from 'react-bootstrap';
+import { Label, Table, Button, Glyphicon } from 'react-bootstrap';
+
+import './Application.css'
 
 const applist = [1, 2, 3, 4, 5];
-
 
 interface Platform {
   id: number;
@@ -17,12 +18,16 @@ class Application extends React.Component {
     return (
       <div className="container-fluid">
         <div>
-          <h2><Label bsStyle="primary">Platform</Label></h2>
+          <h2>
+            <Label bsStyle="primary">Platform</Label>
+            <Button className="add-right"><Glyphicon glyph="plus" /></Button>
+          </h2>
           <Table striped bordered condensed hover>
             <thead>
               <tr>
                 <th>#</th>
                 <th>name</th>
+                <th>operation</th>
               </tr>
             </thead>
             <tbody>
@@ -31,13 +36,17 @@ class Application extends React.Component {
                   <tr key={platform.id}>
                     <td>{platform.id}</td>
                     <td>{platform.name}</td>
+                    <td><Button bsStyle="link">Modify</Button></td>
                   </tr>);
               })}
             </tbody>
           </Table>
         </div>
         <div>
-          <h2><Label bsStyle="primary">Applications</Label></h2>
+          <h2>
+            <Label bsStyle="primary">Applications</Label>
+            <Button className="add-right"><Glyphicon glyph="plus" /></Button>
+          </h2>
           {applist.map(x => <AppItem key={x} />)}
         </div>
       </div>
